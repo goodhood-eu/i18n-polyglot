ad3515467eaccc991a7fe00ee586e54c9e08d5b6
Max Degterev <max@degterev.me>
2020-07-14 15:50:34 +0200

    WIP

---
+++ src/t.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+import { useT } from './hooks';
+
+const T = ({ name, ...props }) => useT()(name, props);
+
+T.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default T;
