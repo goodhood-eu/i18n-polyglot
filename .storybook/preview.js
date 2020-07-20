7c3b112cc9c7332c9cc9ce744f610b551a984964
Max Degterev <max@degterev.me>
2020-07-20 18:07:14 +0200

    the live

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,7 +10,7 @@ const locale = {
     nested: {
       text: 'Hallo nochmal!',
     },
-    substitution: 'I have come here to chew %{variable_1} and kick %{variable_2}... and I\'m all out of %{variable_1}. BAM! *begins to kick %{variable_2}!*',
+    substitution: 'I have come here to chew %{variable_1} and kick %{variable_2}... and I\'m all out of %{variable_1}.',
   },
 };
 
