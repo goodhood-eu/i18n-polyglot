1076df25ac8754453a86b8773d7f93248d90ea79
Max Degterev <max@degterev.me>
2020-07-21 17:13:46 +0200

    minor:

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
-export { default as withLocale } from './hoc';
 export * from './hooks';
 export { default } from './components/international';
 export { default as T } from './components/t';
+export { default as tjsx } from './smart_t';
+
+// Legacy exports to be removed in future versions
+export { default as withLocale } from './hoc';
+export { default as LocaleContext } from './context';
