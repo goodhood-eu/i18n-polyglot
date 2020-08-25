99079d6634cbc27ddf21c6cef7fdf61bf7715eb8
Max Degterev <max@degterev.me>
2020-08-25 18:53:08 +0200

    wip

--- src/components/international.jsx
+++ src/components/international.jsx
@@ -6,14 +6,18 @@ import { Provider } from '../context';
 // Left like this for uglify to be able to clean up
 const warn = (...args) => console.error(...args);
 
+const editorT = (key) => `{{__phrase_${key}__}}`;
+
 const createInstance = ({ type: locale, dictionary: phrases }) => {
   const polyglot = new Polyglot({ warn, locale, phrases });
   return polyglot.t.bind(polyglot);
 };
 
-const International = ({ locale, children }) => {
+const createDebug = () => ({ t: editorT });
+
+const International = ({ locale, projectId, children }) => {
   const context = useMemo(() => {
-    const t = createInstance(locale || {});
+    const t = projectId ? createDebug() : createInstance(locale || {});
     return { t, locale };
   }, [locale]);
 
@@ -25,6 +29,7 @@ International.propTypes = {
     type: PropTypes.string,
     dictionary: PropTypes.object,
   }),
+  projectId: PropTypes.string,
   children: PropTypes.node,
 };
 
